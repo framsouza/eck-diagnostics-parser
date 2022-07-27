6487ce60be680d96c24a4e75f65eb84d5fefa8b6
framsouza <fram.souza14@gmail.com>
2022-07-27 17:25:02 +0200

    removing container condition

--- pkg/resources/pods.go
+++ pkg/resources/pods.go
@@ -30,42 +30,41 @@ func Pods() {
 			fmt.Fprintf(w, "\n%s\t", config.Items[i].Metadata.Name)
 			fmt.Fprintf(w, "\t%s\t", config.Items[i].Metadata.Namespace)
 			fmt.Fprintf(w, "\t%s\t", config.Items[i].Status.Phase)
-			for r := range config.Items[i].Spec.Containers {
-				if config.Items[i].Spec.Containers[r].Resources.Requests.Memory == "" {
-					fmt.Fprintf(w, "\t%s\t", "Not set")
-				} else {
-					fmt.Fprintf(w, "\t%s\t", config.Items[i].Spec.Containers[r].Resources.Requests.Memory)
-				}
-
-				if config.Items[i].Spec.Containers[r].Resources.Limits.Memory == "" {
-					fmt.Fprintf(w, "\t%s\t", "Not set")
-				} else {
-					fmt.Fprintf(w, "\t%s\t", config.Items[i].Spec.Containers[r].Resources.Limits.Memory)
-				}
-
-				if config.Items[i].Spec.Containers[r].Resources.Requests.Cpu == "" {
-					fmt.Fprintf(w, "\t%s\t", "Not set")
-				} else {
-					fmt.Fprintf(w, "\t%s\t", config.Items[i].Spec.Containers[r].Resources.Requests.Cpu)
-				}
-
-				if config.Items[i].Spec.Containers[r].Resources.Limits.Cpu == "" {
-					fmt.Fprintf(w, "\t%s\t", "Not set")
-				} else {
-					fmt.Fprintf(w, "\t%s\t", config.Items[i].Spec.Containers[r].Resources.Limits.Cpu)
-
-				}
-				fmt.Fprintf(w, "\t%s\t", config.Items[i].Spec.NodeName)
-
+			//for r := range config.Items[i].Spec.Containers {
+			if config.Items[i].Spec.Containers[0].Resources.Requests.Memory == "" {
+				fmt.Fprintf(w, "\t%s\t", "Not set")
+			} else {
+				fmt.Fprintf(w, "\t%s\t", config.Items[i].Spec.Containers[0].Resources.Requests.Memory)
 			}
 
-			//Check initcontainers
+			if config.Items[i].Spec.Containers[0].Resources.Limits.Memory == "" {
+				fmt.Fprintf(w, "\t%s\t", "Not set")
+			} else {
+				fmt.Fprintf(w, "\t%s\t", config.Items[i].Spec.Containers[0].Resources.Limits.Memory)
+			}
 
-			//for c := range config.Items[i].Status.InitContainerStatuses {
-			//	fmt.Fprintf(w, "%s %s, ", config.Items[i].Status.InitContainerStatuses[c].Name, config.Items[i].Status.InitContainerStatuses[c].State.Terminated.Reason)
+			if config.Items[i].Spec.Containers[0].Resources.Requests.Cpu == "" {
+				fmt.Fprintf(w, "\t%s\t", "Not set")
+			} else {
+				fmt.Fprintf(w, "\t%s\t", config.Items[i].Spec.Containers[0].Resources.Requests.Cpu)
+			}
 
-			//}
+			if config.Items[i].Spec.Containers[0].Resources.Limits.Cpu == "" {
+				fmt.Fprintf(w, "\t%s\t", "Not set")
+			} else {
+				fmt.Fprintf(w, "\t%s\t", config.Items[i].Spec.Containers[0].Resources.Limits.Cpu)
+
+			}
+			fmt.Fprintf(w, "\t%s\t", config.Items[i].Spec.NodeName)
 
 		}
+
+		//Check initcontainers
+
+		//for c := range config.Items[i].Status.InitContainerStatuses {
+		//	fmt.Fprintf(w, "%s %s, ", config.Items[i].Status.InitContainerStatuses[c].Name, config.Items[i].Status.InitContainerStatuses[c].State.Terminated.Reason)
+
+		//}
+
 	}
 }
