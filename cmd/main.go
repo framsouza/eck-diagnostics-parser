5c04550d7aae0060e8a5039e01bee4285526b303
framsouza <fram.souza14@gmail.com>
2022-07-27 11:43:35 +0200

    testing tmp file

--- cmd/main.go
+++ cmd/main.go
@@ -12,7 +12,7 @@ import (
 
 var (
 	zipfile     = flag.String("zipfile", "", "Enter zip file name")
-	destination = "./tmp/"
+	destination = "/tmp/"
 )
 
 func main() {
@@ -43,7 +43,7 @@ func main() {
 	resources.StorageClass()
 	summary()
 	// clean up destination dir
-	//os.RemoveAll(destination)
+	os.RemoveAll(destination)
 
 }
 
